2e96660785e49cea6053e444beda8c32aee3f65c
gopivaibhav <gopivaibhav0@gmail.com>
2021-08-03 14:50:18 +0530

    added manual

--- bgv.py
+++ bgv.py
@@ -11,8 +11,11 @@ from pyautogui import click, keyDown, keyUp
 from keyboard import press,write
 from time import sleep
 from googletrans import Translator
+from PyDictionary import PyDictionary
 import speedtest
 import eel
+import random
+import os
 eel.init("project")
 
 
@@ -41,7 +44,6 @@ def get_audio():
             eel.addText("You said: "+speech)
         except sr.UnknownValueError:
             x = "Sorry,I couldn't hear you"
-            talk(x)
             eel.addText(x)
         except sr.RequestError:
             talk("Sorry , My API to recognize voice is down")
@@ -54,6 +56,28 @@ def today_date():
     sec = str(datetime.datetime.now().second)
     time=date+" "+hour+"hours "+min+"minutes "+sec+"seconds"
     return time
+
+def get_audio_telugu():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        eel.addText("Listening....")
+        talk("Speak now")
+        audio = r.listen(source)
+        try:
+            global speech
+            speech=" "
+            speech = r.recognize_google(audio,language='te-IN')
+            speech = speech.lower()
+            eel.addText("You said: "+speech)
+        except sr.UnknownValueError:
+            x = "Sorry,I couldn't hear you"
+            talk(x)
+            eel.addText(x)
+        except sr.RequestError:
+            talk("Sorry , My API to recognize voice is down")
+    return speech
+
 def get_audio_hindi():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -75,13 +99,29 @@ def get_audio_hindi():
             talk("Sorry , My API to recognize voice is down")
     return speech
 
-def trans():
+def trans_hindi():
     translator = Translator(service_urls=['translate.googleapis.com'])
-    talk("what to translate")
+    eel.addText("Vaibhav Assistant : What should I translate")
+    talk("what Should I translate")
     trans_ = get_audio_hindi()
     results = translator.translate(trans_)
     Text = results.text
-    return Text
+    return Text+" is the translated text"
+
+def trans_telugu():
+    translator = Translator(service_urls=['translate.googleapis.com'])
+    eel.addText("Vaibhav Assistant : What should I translate")
+    talk("what Should I translate")
+    trans_ = get_audio_telugu()
+    results = translator.translate(trans_)
+    Text = results.text
+    return Text+" is the translated text"
+
+def coin_toss():
+    eel.addText("Vaibhav Assistant : I am going to flip coin......!")
+    talk("I am going to flip coin")
+    result = random.choice(["Heads","Tails"])
+    return result
 
 def weather(location):
     user_api =  "551fa158b9c984913a5f0b943e7e8578"
@@ -178,56 +218,34 @@ def trail():
             speak =results
                     
     
-        elif "who are you" in text or "define yourself" in text:
+        elif "who are you" in text:
             speak = "Hello, I am Vaibhav Assistant, created by Gopi Vaibhav"
             
         elif "your name" in text:
             speak = "This is Vaibhav Assistant."
     
         elif "who am i" in text:
-            speak = "You might be a human."
+            speak = "You are a Human"
             
-        elif "why do you exist" in text or "why did you come" in text:
-            speak = "It is a secret."
+        elif "why are you here" in text or "why did you come" in text:
+            speak = "Because you activated me."
     
         elif "how are you" in text:
             speak =  "I am fine, Thank you!"
-            speak += "\nHow are you?"
+            speak += "\nWhat about you?"
                     
-        elif "i am good" in text or "i am fine" in text:
+        elif "i am good" in text or "i am fine" in text or "i am nice" in text:
             speak  ="cool , How can i help for you"
-                    
-        elif "i am fine" in text :
-            speak = "It's good to know that you are fine"
-                    
             
         elif "open" in text.lower():                
-            if "youtube" in text.lower():
-                speak = "opening youtube in browser"
-                webbrowser.open("https://youtube.com/")
-            
-            elif "facebook" in text.lower():
-                speak = "opening facebook in browser"
-                webbrowser.open("https://www.facebook.com/")
-
-            elif "instagram" in text.lower():
-                speak = "opening instagram in browser"
-                webbrowser.open("https://www.instagram.com/")
-                
-            elif "google" in text.lower():
-                speak = "opening google in browser"
-                webbrowser.open("https://google.com")
-            
-            elif "whatsapp" in text.lower():
-                speak = "opening whatsapp in browser"
+            if "whatsapp" in text.lower():
+                speak = "opening whatsapp web in browser"
                 webbrowser.open("https://web.whatsapp.com/")
                 
-            elif "github" in text.lower():
-                speak = " opening github"
-                webbrowser.open("https://github.com")
-                
             else:
-                    speak = "No such Application in framework "
+                l=text.lower().split('open ')
+                webbrowser.open("https://"+l[1]+".com")
+                speak = " opening "+l[1]+" in browser"
                 
         elif "youtube" in text.lower():
             ind = text.lower().split().index("youtube")
@@ -249,23 +267,72 @@ def trail():
             webbrowser.open("https://www.google.com/maps/place/"+"+".join(location))
 
         elif "weather" in text or "climate" in text:
-            talk("What's the location?")
             eel.addText("What's the location?")
+            talk("What's the location?")
             Location = get_audio()
             weather(Location)
             speak = 'Here are the weather reports of '+str(Location)    
                         
-        elif "don't listen" in text or "stop listening" in text or "do not listen" in text:
-            talk("for how many seconds do you want me to sleep")
+        elif "don't listen" in text or "do not listen" in text:
+            eel.addText("Vaibhav Assistant : How many seconds do you want me to sleep")
+            talk("How many seconds do you want me to sleep?")
             a = int(get_audio())
             time.sleep(a)
             speak = str(a) + " seconds completed. Now you can ask me anything"
         
-        elif "hindi" in text or "translate" in text:
-            speak=trans()
+        elif "translate" in text:
+            eel.addText("Vaibhav Assistant : What is the language you are going to speak?")
+            talk("What is the language you are going to speak?(Hindi or Telugu)")
+            lang = get_audio()
+            lang = lang.lower()
+            if "hindi" in lang:
+                speak=trans_hindi()
+            else:
+                speak=trans_telugu()
         
         elif "speed" in text:
             speak=SpeedTest()
+        
+        elif "meaning" in text:
+            dictionary=PyDictionary()
+            l=text.split(' of ')
+            meaningword=l[1]
+            out = dictionary.meaning(meaningword)
+            eel.addText("Vaibhav Assistant : Meaning of "+l[1]+" is "+str(out))
+            talk("Meaning of "+l[1]+" is "+str(out))
+
+        elif "synonym" in text:
+            dictionary=PyDictionary()
+            l=text.split(' of ')
+            synonymword=l[1]
+            out = dictionary.synonym(synonymword)
+            eel.addText("Vaibhav Assistant : Synonyms of "+l[1]+" are "+str(out))
+            talk("Synonyms of "+l[1]+" are "+str(out))
+
+        elif "antonym" in text:
+            dictionary=PyDictionary()
+            l=text.split(' of ')
+            antonymword=l[1]
+            out = dictionary.antonym(antonymword)
+            eel.addText("Vaibhav Assistant :  Antonyms of "+l[1]+" are "+str(out))
+            talk("Antonyms of "+l[1]+" are "+str(out))
+
+        elif "toss" in text or "flip" in text:
+            speak=coin_toss()
+            
+
+        elif "shutdown" in text :
+            eel.addText("Vaibhav Assistant : Would you like to shutdown PC?")
+            talk("Would you like to shutdown PC")
+            shutdown = get_audio()
+  
+            if shutdown == 'no':
+                eel.addText("Vaibhav Assistant : Request Cancelled")
+                talk("request cancelled")
+                exit()
+            else:
+                talk("Shutting down PC")
+                os.system("shutdown /s /t 1")
 
         elif "whatsapp" in text :
             if "message" in text:
@@ -289,7 +356,7 @@ def trail():
                 
         elif "goodbye" in text or "bye" in text or "quit" in text or "exit" in text:
             eel.addText("Terminating Vaibhav Assistant")
-            talk("Terminating Vaibhav Assistant")
+            talk("......Terminating Vaibhav Assistant......")
             break 
         
         eel.addText("Vaibhav Assistant : "+speak)
@@ -297,4 +364,4 @@ def trail():
         
 
 
-eel.start("project.html",size=(500, 500))
+eel.start("project.html",size=(1000, 1000))
--- project/project.js
+++ project/project.js
@@ -8,3 +8,31 @@ function addText(msg){
     data.innerHTML+="<br>"+msg;
 }
 eel.expose(addText);
+
+function toggle(c){
+    var manual=document.getElementById(c);
+    if(manual.style.display=='absolute'){
+        manual.style.display='none'   
+    }else{
+        manual.style.display='block'
+    }
+    console.log(manual.style)
+}
+
+function Show() {
+    let manual = document.getElementById("min");
+    
+    let btn=document.getElementById("man")
+    if (btn.innerText=="Manual") {
+        manual.style.display = "block";
+        btn.innerText="Close";
+        manual.style.animationName="wohoo";
+        manual.style.animationDuration=".5s";
+    } else {
+        btn.innerText="Manual"
+        manual.style.animationName="yesss";
+        manual.style.animationDuration=".5s";
+        
+    }
+    console.log(btn.innerText)
+}
